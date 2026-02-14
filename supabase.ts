8655923bb1a19790dd683a7e97714343623dcf50
SydneyMico <sydneymico13@gmail.com>
2026-02-14 17:47:20 +0200

    added env protection

--- supabase.ts
+++ supabase.ts
@@ -1,8 +1,6 @@
+import { createClient } from "@supabase/supabase-js";
 
-import { createClient } from '@supabase/supabase-js';
-
-// Verified connection parameters
-const supabaseUrl = 'https://isehhrtbccfllbnmocoy.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlzZWhocnRiY2NmbGxibm1vY295Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg5OTI2MDAsImV4cCI6MjA4NDU2ODYwMH0.a3wD6scbjSqIj_d6PHquZcB7o6wKDd77HBwsZcNP4Kc';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
